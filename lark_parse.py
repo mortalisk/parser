44d28451436251973fddadbea60dc64eed7dab45
Morten Bendiksen <morten.bendiksen@gmail.com>
2023-01-13 18:06:57 +0100

    experimenting

--- lark_parse.py
+++ lark_parse.py
@@ -1,26 +1,36 @@
 from lark import Lark
-grammar = """
+grammar = r"""
 
 %import common.ESCAPED_STRING   -> STRING
 %import common.CNAME
 %import common.SIGNED_NUMBER    -> NUMBER
+%import common.NEWLINE          -> NEWLINE
 
-start: statement+
-statement: CNAME CNAME
+
 arg: CNAME | NUMBER | arglist | STRING 
 
 arglist: CNAME kw_list
 kw_list: "(" [ kw_pair ("," kw_pair)*] ")"
-kw_pair: kw_name "=" STRING
+kw_val: arg
+kw_pair: kw_name "=" kw_val
 kw_name: "<" CNAME ">"
 
-comment: "//" STRING*
+COMMENT: "--" /[^\n]*/
 
+start: instruction+
 
+inst: "TEST" arg* -> test
+    | "DEFINE" kw_name arg -> define
+
+instruction: inst NEWLINE
+
+WHITESPACE: (" ")+
+%ignore WHITESPACE
+%ignore COMMENT
 
 """
-
+#%import common._STRING_ESC_INNER -> UNQUOTED
 with open("snake_oil.ert") as f:
-    parser = Lark(grammar)
+    parser = Lark(grammar, propagate_positions=True)
     tree = parser.parse(f.read())
-    print(tree)
+    print(tree.pretty())
